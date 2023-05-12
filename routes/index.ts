fbeb3e2b0ceb6eb89c980120922531846178986e
Santiago Di Deo <101676324+SantiagoDiDeo@users.noreply.github.com>
2023-05-11 23:05:16 -0300

    update & delete

--- routes/index.ts
+++ routes/index.ts
@@ -1,7 +1,9 @@
 import { Router } from "../deps.ts";
-import { createProduct, findProduct, listProducts } from "../handlers/products.ts";
+import { createProduct, findProduct, listProducts, updateProduct, deleteProduct} from "../handlers/products.ts";
 
 export const productRouter = new Router()
   .get('/api/products/:productId', findProduct)
+  .delete('/api/products/:productId', deleteProduct)
+  .put('/api/products/:productId', updateProduct)
   .get('/api/products', listProducts)
   .post('/api/products', createProduct);
